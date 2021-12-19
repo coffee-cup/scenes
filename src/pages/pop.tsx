dacd9c23da56820b4e0ef45611f0fcb28047ad1a
Jake Runzer <jakerunzer@gmail.com>
2021-12-19 10:59:56 -0700

    lil drawing balls

--- src/pages/pop.tsx
+++ src/pages/pop.tsx
@@ -1,25 +1,27 @@
 import { Stats } from "@react-three/drei";
-import { LightProps, MeshProps, useFrame } from "@react-three/fiber";
-import React, { useRef } from "react";
+import {
+  Euler,
+  LightProps,
+  MeshProps,
+  useFrame,
+  useThree,
+  Vector3,
+} from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
 import { Light, Mesh } from "three";
+import * as THREE from "three";
 import "twin.macro";
 import { Scene } from "../components/Scene";
 import { Page } from "../layouts/Page";
+import { rand } from "../utils";
 
 export const Pop: React.FC = () => {
   return (
     <Page>
-      <Scene tw="border-2 border-dotted border-violet-900" camera={[1, 4, 6]}>
+      <Scene tw="border-2 border-dotted border-violet-900" camera={[0, 0, 20]}>
         <Lights />
 
-        <RotatingBox position={[-2, 0, 0]} castShadow />
-        <SphereItem position={[1, 0, 0]} castShadow />
-
-        <Plane
-          position={[-1, -2, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          receiveShadow
-        />
+        <BallGenerator />
 
         {/* <Stats /> */}
       </Scene>
@@ -28,6 +30,72 @@ export const Pop: React.FC = () => {
 };
 
 export default Pop;
+
+export interface Ball {
+  id: number;
+  position: Vector3;
+  scale: number;
+  born: number; // seconds
+}
+
+let ballId = 0;
+
+const FREQ = 0.5; // seconds
+
+const min = -20;
+const max = 20;
+
+const randomBall = (): Ball => {
+  return {
+    id: ballId++,
+    position: [rand(min, max), rand(min, max), rand(min, max)],
+    scale: rand(0.5, 1.5),
+    born: new Date().getTime(),
+  };
+};
+
+const BallGenerator: React.FC = () => {
+  const [balls, setBalls] = useState<Ball[]>([]);
+  const count = useRef(0);
+
+  useFrame(({ mouse, camera, viewport, clock }) => {
+    if (clock.elapsedTime > count.current * FREQ) {
+      const vector = new THREE.Vector3(
+        mouse.x,
+        mouse.y,
+        camera.position.z + 0.5,
+      );
+      vector.unproject(camera);
+      const dir = vector.sub(camera.position).normalize();
+      const distance = camera.position.z - 5 - camera.position.z / dir.z;
+      const pos = camera.position.clone().add(dir.multiplyScalar(distance));
+
+      count.current += 1;
+      const newBall = { ...randomBall(), position: pos };
+      setBalls(balls => [...balls, newBall]);
+    }
+  });
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const newBall = randomBall();
+  //     const now = new Date().getTime();
+  //     setBalls(balls => [...balls, newBall]);
+  //   }, FREQ);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+
+  return (
+    <>
+      {balls.map(b => (
+        <SphereItem key={b.id} position={b.position} scale={b.scale} />
+      ))}
+    </>
+  );
+};
 
 const Lights: React.FC<LightProps> = props => {
   const ref = useRef<Light>(null!);
---
+++ src/utils.ts
@@ -0,0 +1,1 @@
+export const rand = (min = 0, max = 1): number => Math.random() * max + min;
